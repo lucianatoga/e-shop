0a9b7ee20fbe37e7f6ea1c99de9575ca52266749
Luciana Torres <garciatorresluciana@gmail.com>
2024-12-06 13:56:43 -0300

    finished search bar functionality

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const search_section=document.getElementById("search-section");
-
-const search_button=document.getElementById("search-button");
-search_button.addEventListener('click', ()=>{
+function showSearchSection(){
+    document.getElementById("title-buttons-section").style.display="none";
+    document.getElementById("option-menu-section").style.display="none";
     search_section.style.display="flex";
     search_section.style.position="absolute";
-    search_section.style.top="5rem";
-    search_section.style.zIndex="100"
-
-})
-const close_button=document.getElementById("close-button");
-close_button.addEventListener('click', ()=>{
+    search_section.style.top="0";
+    document.querySelector("main").classList.add("disabled");
+    document.querySelector("body").style.overflow="hidden";
+}
+function closeSearchSection(){
     search_section.style.display="none";
-})
+    document.getElementById("title-buttons-section").style.display="flex";
+    document.getElementById("option-menu-section").style.display="flex";
+    document.querySelector("main").classList.remove("disabled");
+    document.querySelector("body").style.overflow="auto";
+}
+const search_button=document.getElementById("search-button");
+search_button.addEventListener('click', showSearchSection);
+const close_button=document.getElementById("close-button");
+close_button.addEventListener('click', closeSearchSection)
 
 const search_bar_input=document.querySelector('input[name="search"]');
 search_bar_input.addEventListener('focus', ()=>{
