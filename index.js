11adac5bc9124fe24a3d0a247aa8249fb7e108b9
Luciana Torres <garciatorresluciana@gmail.com>
2025-01-10 17:37:38 -0300

    made function to add dinamically the products cards

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//SEARCH SECTION//
+//SEARCH SECTION// 
 const search_section=document.getElementById("search-section");
 let search_visible=false;
 function showSearchSection(){
@@ -47,9 +47,9 @@ search_bar_input.addEventListener('blur', ()=>{
 
 //NAV MENU//
 let dropdown_visible=false;
-const links=Array.from(document.querySelectorAll("ul.navbar a"));
+const button_links=Array.from(document.querySelectorAll('ul.navbar a[role="button"]'));
 const all_dropdowns=document.querySelectorAll("ul.dropdown-content");
-links.forEach(link=>link.addEventListener('click', showHideDropdown));
+button_links.forEach(link=>link.addEventListener('click', showHideDropdown));
 
 function showHideDropdown(){ 
     event.preventDefault();
@@ -76,7 +76,7 @@ function showHideDropdown(){
 
 window.addEventListener('click', function(event){
     // CLOSE DROPDOWNS WHEN CLICKING OUTSIDE
-    if(dropdown_visible===true && !links.includes(event.target)){
+    if(dropdown_visible===true && !button_links.includes(event.target)){
         
         all_dropdowns.forEach(dropdown=>{
             if(dropdown.classList.contains("dropdown-content-visible")){
@@ -111,7 +111,111 @@ window.addEventListener('scroll', function(){
     
 })
 
-//DISPLAY PRODUCTS CARDS
-const calzado_section=document.getElementById("calzado-cards-container");
+//BODY
 
-//CHANGE PICTURE WHEN HOVERING OVER PRODUCT CARD
+const current_discount=15;
+//const products_section=document.querySelector("products-section");
+//GET PRODUCTS FROM JSON
+
+document.addEventListener('DOMContentLoaded', async function(){
+        const response=await fetch("/productos.json");
+        const json= await response.json();
+        //DISPLAY PRODUCTS CARDS ON HOME PAGE
+        if(document.getElementById("previews-home-container")){
+            showFourProducts(json.calzado,"botas", "four-cards-container1");
+            showFourProducts(json.aros, "plata", "four-cards-container2");
+        }
+        // if(document.querySelector("section.products-section")){
+        //     //console.log(document.title);
+        //     const subcategory=document.title;
+        //     const category="calzado";
+        //     showProducts(subcategory,json[category]);
+        // }
+        
+        const params= new URLSearchParams(window.location.search);
+        const category=params.get('category');
+        const subcategory=params.get('subcategory');
+        if(category&&subcategory){
+            //console.log(category, subcategory);
+            showProducts(subcategory,json[category]);
+        }
+})
+const dropdown_links=Array.from(document.querySelectorAll('ul.dropdown-content a'));
+        dropdown_links.forEach(link => link.addEventListener('click', function(e){
+            //e.preventDefault();
+            const subcategory=link.textContent.split(" ")[0].toLowerCase();
+            const category=link.closest('ul.dropdown-content').previousElementSibling.textContent.toLowerCase();
+            const url=`${link.getAttribute('href')}?category=${category}&subcategory=${subcategory}`;
+            //console.log(url);
+            link.href=url;
+            //window.location.href=url;
+        }));
+function showFourProducts(data, category, section){
+    const card_section=document.getElementById(section);
+    const products_in_category=data.filter(product=>product.category===category);
+    (products_in_category.slice(0,4)).forEach(product=> {
+        const discounted_price=product.price-((product.price*current_discount)/100);
+        const product_card=document.createElement('div');
+        product_card.innerHTML=`
+        <img src="${product.img[0]}" alt="" height="100%">
+        <p>${product.name}</p>
+        <div class="prices"><s>$${product.price}</s><p>$${discounted_price}</p></div>
+        `;
+        product_card.className="product-card";
+        //CHANGE PICTURE WHEN HOVERING OVER PRODUCT CARD
+        product_card.addEventListener('mouseover', function(event){
+            const card=event.target;
+            if(product.img.length>1){
+                card.src=product.img[1];
+            }
+        });
+        product_card.addEventListener('mouseout', function(event){
+            const card=event.target;
+            if(product.img.length>1){
+                card.src=product.img[0];
+            }
+        });
+        card_section.appendChild(product_card);
+    })
+}
+
+//PRODUCTS PAGES
+
+function showProducts(subcategory, products){
+    const products_section=document.getElementById(subcategory+"-section");
+    products_section.querySelector('h2').textContent=subcategory.toUpperCase();
+    const products_cards_conatiner=products_section.querySelector('section.products-cards-conatiner');
+    //console.log(products_section);
+    if(subcategory==="todos"){
+        products.forEach(product=>{
+            const discounted_price=product.price-((product.price*current_discount)/100);
+            const product_card=document.createElement('div');
+            product_card.innerHTML=`
+            <img src="../${product.img[0]}" alt="" height="100%">
+            <p>${product.name}</p>
+            <div class="prices"><s>$${product.price}</s><p>$${discounted_price}</p></div>
+            `;
+            product_card.className="product-card";
+            products_cards_conatiner.appendChild(product_card);
+        })
+    }
+    else{
+        products.forEach(product=>{
+            if(product.category===subcategory){
+                const discounted_price=product.price-((product.price*current_discount)/100);
+                const product_card=document.createElement('div');
+                product_card.innerHTML=`
+                <img src="../${product.img[0]}" alt="" height="100%">
+                <p>${product.name}</p>
+                <div class="prices"><s>$${product.price}</s><p>$${discounted_price}</p></div>
+                `;
+                product_card.className="product-card";
+                products_cards_conatiner.appendChild(product_card);
+            }
+        })
+    }
+}
+//add products to json
+//show products in all pages
+//add the hover over functionalities
+//add responsiveness
